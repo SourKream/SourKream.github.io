fe865f1713206fcf1657a9635365a34c3aa341e1
Shantanu Kumar <kumarshantanu95@gmail.com>
2019-02-22 22:39:23 -0800

    Fixed the maze post finally

--- P5/maze.js
+++ P5/maze.js
@@ -1,168 +1,238 @@
+// Configurations
+let canvasSize = 600;
+let gridSize = 53;
+let cellSize = canvasSize / gridSize;
+let percentageFill = 25;
 
-let gridSize = 75;
-let cellSize = 8;
+let start = new Element(2, 2, 0);
+let goal = new Element(gridSize -2, gridSize -2, 0);
 
-var grid = new Array(gridSize);
+// Logic Variables
+var maze;
 var search;
-var prev = 0;
-var GlobalGoal = new Element(gridSize -2, gridSize -2, 0);
+var searchType = 2;
+
+// Runtime Control Variables
+var paused = false;
 
 function setup(){
-  var canvas = createCanvas(600, 600);
+  var canvas = createCanvas(canvasSize + 1, canvasSize + 1);
   canvas.parent('maze_holder');
   
-  // Random Initilise Maze
-  for (var i=0; i<gridSize; i++)
-    grid[i] = new Array(gridSize);
-  randomInitialise();
-
-  grid[GlobalGoal.x][GlobalGoal.y] = 10;
-  search = new Search(grid,2,2, GlobalGoal);
+  maze = new Maze(gridSize, cellSize, percentageFill);
+  maze.randomInit(percentageFill);
+  maze.setGoal(goal)
+  search = new Search(maze, start, goal, searchType);
 }
 
 function draw() {
   background(220);
-  drawMaze();
-  prev = search.iterate(prev);
+  maze.draw();
+  if (!paused && !search.completed) {
+    search.iterate();
+  }
 }
 
 function reset() {
-  search = 0;
-  for (var i=0; i<gridSize; i++){
-    for (var j=0; j<gridSize; j++){
-      if (grid[i][j] != 1)
-        grid[i][j] = 0;
-    }
-  }
-  grid[GlobalGoal.x][GlobalGoal.y] = 10;
-  search = new Search(grid,2,2, GlobalGoal);
+  maze.clean();
+  maze.setGoal(goal)
+  search = new Search(maze, start, goal, searchType);
 }
 
-function drawMaze() {
-  // Draw Maze  
-  strokeWeight(1);
-  stroke(192,192,192);
-  for (var i=0; i<gridSize; i++){
-    for (var j=0; j<gridSize; j++){
-      if (grid[i][j] == 1)
-        fill(0);
-      else if (grid[i][j] == 0)
-        fill(255);
-      else if (grid[i][j] == 5)
-        fill(255,0,0);
-      else if (grid[i][j] == 3)
-        fill(0,0,255);
-      else if (grid[i][j] == 10)
-        fill(0,255,0);
-      rect(i*cellSize, j*cellSize, cellSize, cellSize);
-    }
-  } 
-}
-
-function getNeighbours(element){
-  var neighbours = [];
-  var i = element.x;
-  var j = element.y;
-  if(i+1 < gridSize)
-    neighbours.push(new Element(i+1, j, element));
-  if(j+1 < gridSize)
-    neighbours.push(new Element(i, j+1, element));
-  if(i-1 >= 0)
-    neighbours.push(new Element(i-1, j, element));
-  if(j-1 >= 0)
-    neighbours.push(new Element(i, j-1, element));
-  return neighbours
-}
+////////////////////////////////////////
+/// Interactions with the maze
 
 function keyPressed() {
-  print(key)
   if (key == 'P'){
-  }
-  else if (key == 'R'){
+    paused = !paused;
+  } else if (key == 'R'){
+    reset();
+  } else if (key == 'V'){
+    randomGrid();
+  } else if (key == 'B'){
+    blankGrid();
+  } else if (key == '1'){
+    searchType = 0;
+    reset();
+  } else if (key == '2'){
+    searchType = 1;
+    reset();
+  } else if (key == '3'){
+    searchType = 2;
     reset();
   }
-  else if (key == 'T'){
-  }
-  else if (key == 'W'){
-  }
-  else if (key == 'C'){
-  }
-  else if (key == '1'){
-  }
 }
 
-// function mouseDragged() {
-//   if ((mouseX > 0) && (mouseX < gridSize*cellSize))
-//     if ((mouseY > 0) && (mouseY < gridSize*cellSize)){
-//       if (keyIsPressed == true && keyCode == SHIFT)
-//           grid[floor(mouseX/cellSize)][floor(mouseY/cellSize)] = false; 
-//       else
-//         grid[floor(mouseX/cellSize)][floor(mouseY/cellSize)] = true;       
-//     }
-// }
-
-// function mouseClicked() {
-//   if ((mouseX > 0) && (mouseX < gridSize*cellSize))
-//     if ((mouseY > 0) && (mouseY < gridSize*cellSize))
-//       grid[floor(mouseX/cellSize)][floor(mouseY/cellSize)] = !grid[floor(mouseX/cellSize)][floor(mouseY/cellSize)]; 
-// }
-
-// function cleanGrid(){
-//   for (var i=0; i<gridSize; i++)
-//     for (var j=0; j<gridSize; j++)
-//         grid[i][j] = false;
-// }
-
-function randomInitialise() {
-  for (var i=0; i<gridSize; i++){
-    for (var j=0; j<gridSize; j++){
-      if (random(100) <= 30)
-        grid[i][j] = 1;
+function mouseDragged() {
+  if ((mouseX > 0) && (mouseX < gridSize*cellSize))
+    if ((mouseY > 0) && (mouseY < gridSize*cellSize)){
+      var i = floor(mouseX/cellSize);
+      var j = floor(mouseY/cellSize);      
+      if (keyIsPressed == true && keyCode == SHIFT)
+        maze.grid[i][j] = 0; 
       else
-        grid[i][j] = 0;
+        maze.grid[i][j] = 1;       
+    }
+}
+
+function mouseClicked() {
+  if ((mouseX > 0) && (mouseX < gridSize*cellSize))
+    if ((mouseY > 0) && (mouseY < gridSize*cellSize)) {
+      var i = floor(mouseX/cellSize);
+      var j = floor(mouseY/cellSize);
+      if (maze.grid[i][j] == 0) {
+        maze.grid[i][j] = 1;
+      } else if (maze.grid[i][j] == 1) {
+        maze.grid[i][j] = 0;        
+      } 
+    }
+}
+
+function blankGrid(){
+  maze.blank();
+  reset();
+}
+
+function randomGrid(){
+  maze.randomInit(percentageFill);
+  reset();
+}
+
+////////////////////////////////////////
+/// Class to hold the maze/grid
+
+function Maze(gridSize, cellSize, ) {
+
+  this.gridSize = gridSize;
+  this.cellSize = cellSize;
+  this.grid = new Array(this.gridSize);
+  for (var i=0; i<this.gridSize; i++)
+    this.grid[i] = new Array(this.gridSize);
+
+  this.randomInit = function(percentageFill) {
+    for (var i=0; i<this.gridSize; i++){
+      for (var j=0; j<this.gridSize; j++){
+        if (random(100) <= percentageFill)
+          this.grid[i][j] = 1;
+        else
+          this.grid[i][j] = 0;
+      }
+    }
+    for (var i=0; i<5; i++) {
+      for (var j=0; j<5; j++) {
+          this.grid[i][j] = 0;
+      }
     }
   }
+
+  this.setGoal = function(goal) {
+    this.grid[goal.x][goal.y] = 10;
+  }
+
+  this.clean = function() {
+    for (var i=0; i<this.gridSize; i++){
+      for (var j=0; j<this.gridSize; j++){
+        if (this.grid[i][j] != 1) {
+          this.grid[i][j] = 0;
+        }
+      }
+    }    
+  }
+
+  this.blank = function() {
+    for (var i=0; i<this.gridSize; i++){
+      for (var j=0; j<this.gridSize; j++){
+          this.grid[i][j] = 0;
+      }
+    }    
+  }
+
+  // Draw Maze  
+  this.draw = function() {
+    strokeWeight(1);
+    stroke(192,192,192);
+    if (paused) {
+      stroke(192, 0, 0);
+    }
+    for (var i=0; i<this.gridSize; i++){
+      for (var j=0; j<this.gridSize; j++){
+        if (this.grid[i][j] == 1)
+          fill(0);                  // Black
+        else if (this.grid[i][j] == 0)
+          fill(255);                // White
+        else if (this.grid[i][j] == 5)
+          fill(255,0,0);            // Red
+        else if (this.grid[i][j] == 3)
+          fill(0,0,255);            // Blue
+        else if (this.grid[i][j] == 10)
+          fill(0,255,0);            // Green
+        rect(i*this.cellSize, j*this.cellSize, this.cellSize, this.cellSize);
+      }
+    } 
+  }
+
 }
 
+////////////////////////////////////////
+/// General Maze Search Algorithm
 
-function Search(grid, i, j, goal){
+function Search(maze, start, goal, searchType){
 
-  this.grid = grid;
+  this.grid = maze.grid;
+  this.gridSize = maze.gridSize;
   this.goal = goal;
-  this.startElement = new Element(i, j, 0);
-  // this.searchFront = new DFS(this.startElement);
-  // this.searchFront = new BFS(this.startElement);
-  this.searchFront = new InformedSearch(this.startElement, this.goal);
+  this.startElement = start;
+  this.previousCurrent = 0;
+  this.completed = false;
 
-  this.iterate = function(previous) {
-    if (previous != 0)
-      this.colourPathToElement(previous, 5);
+  if (searchType == 0) {
+    this.searchFront = new DFS(this.startElement);
+  } else if (searchType == 1) {
+    this.searchFront = new BFS(this.startElement);
+  } else {
+    this.searchFront = new InformedSearch(this.startElement, this.goal);
+  }
+
+  this.getNeighbours = function(element) {
+    var neighbours = [];
+    var i = element.x;
+    var j = element.y;
+    if(i+1 < this.gridSize)
+      neighbours.push(new Element(i+1, j, element));
+    if(j+1 < this.gridSize)
+      neighbours.push(new Element(i, j+1, element));
+    if(i-1 >= 0)
+      neighbours.push(new Element(i-1, j, element));
+    if(j-1 >= 0)
+      neighbours.push(new Element(i, j-1, element));
+    return neighbours
+  }
+
+  this.iterate = function() {
+    this.colourPathToElement(this.previousCurrent, 5);
 
     curr = this.searchFront.getElement();
     i = curr.x;
     j = curr.y;
   
     if (this.grid[i][j] == 10){
-      print("DONE")
-      this.colourPathToElement(previous, 5);
-      this.colourPathToElement(curr, 3);
-      noLoop();
+      this.colourPathToElement(curr, 10);
+      this.completed = true;
       return;
     } 
 
     if(this.grid[i][j] != 0)
       return;
 
-    this.grid[i][j] = 5;
     this.colourPathToElement(curr, 3);
-    neighbours = getNeighbours(curr);
+    neighbours = this.getNeighbours(curr);
     for (var k=0; k<neighbours.length; k++){
       if (this.grid[neighbours[k].x][neighbours[k].y] == 0 || this.grid[neighbours[k].x][neighbours[k].y] == 10){
         this.searchFront.addElement(neighbours[k]);
       }
     }
 
-    return curr;
+    this.previousCurrent = curr;
   }
 
   this.colourPathToElement = function(element, colour) {
@@ -175,6 +245,8 @@ function Search(grid, i, j, goal){
 
 }
 
+/// Element class to hold this info about a node
+
 function Element(i, j, prev){
 
   this.x = i;
@@ -182,6 +254,9 @@ function Element(i, j, prev){
   this.previous = prev;
 
 }
+
+/// Depth First Search
+/// Implemented using a stack
 
 function DFS(element){
 
@@ -197,6 +272,8 @@ function DFS(element){
 
 }
 
+/// Breadth First Search
+/// Implemented using a queue
 
 function BFS(element){
 
@@ -219,6 +296,9 @@ function BFS(element){
 
 }
 
+/// Informed Search
+/// Implemented using a priority queue
+
 function distElement(a, b) {
   return Math.abs(a.x - b.x) + Math.abs(a.y - b.y)
 }
@@ -227,7 +307,7 @@ function InformedSearch(element, goal){
 
   this.goal = goal;
   this.store = new PriorityQueue({ comparator: function(a, b) { 
-    return distElement(a, GlobalGoal) - distElement(b, GlobalGoal); 
+    return distElement(a, goal) - distElement(b, goal); 
   }});
   this.store.queue(element);
 
--- _posts/2018-02-07-maze-solver.markdown
+++ _posts/2018-02-07-maze-solver.markdown
@@ -7,3 +7,15 @@ div_id: maze_holder
 js_source: ../../../../../P5/maze.js
 ---
 
+You can use these keys to interact with the game,
+
+- `P` : Pause iterations
+- `R` : Restart the search
+- `V` : Randomize the grid
+- `B` : Create a blank grid
+- `1` : Set search algorithm to Deapth First Search
+- `2` : Set search algorithm to Breadth First Search
+- `3` : Set search algorithm to Informed Search
+
+You can also use the mouse to drag and draw your own maze on the board when the iterations are paused. 
+Use the `SHIFT` key while dragging to erase.
